056950db3c346740d6473f974dcc3a09572924c2
kirubel <kirubel>
2022-09-14 05:40:48 -0700

    task 4

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints exactly and that piece of art is useful
+ *
+ * Description: using the main function
+ * program  prints exactly and that piece of art is useful
+ * Return: 1
+ */
+
+int main(void)
+{
+	char str [] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+
+	puts(str);
+	return (1);
+}
--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,17 @@
 #include <stdio.h>
 
 /**
- * main - print the string in the put function
+ * main - print the string in the puts function
  *
  * Description: using the main function
  * this program prints "programming is like building a multilingual puzzle
  * Return: 0
  */
+
 int main(void)
 {
-	puts("\"programming is like building a multilingual puzzle");
+	char str[] = "\"programming is like building a multilingual puzzle";
+
+	puts(str);
 	return (0);
 }
